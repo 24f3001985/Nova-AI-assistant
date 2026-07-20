c3c72f9526f0c8a9373d76c01335c513f92718ff
24f3001985 <sagarkhushi81@gmail.com>
2026-07-20 23:14:29 +0530

    Final commit

---
+++ musicLibrary.py
@@ -0,0 +1,8 @@
+music ={
+    "pasoori" :"https://youtu.be/5Eqb_-j3FDA?si=LyszBv1dhH2EKWcd" ,
+    "sapphire" : "https://youtu.be/JgDNFQ2RaLQ?si=ddg6_9SllSJ2fORz",
+    "bulleya" : "https://youtu.be/_51KXfwcPMs?si=hK1WPKCnV5UiJV_3" ,
+    "closer": "https://youtu.be/PT2_F-1esPk?si=g5dxFdKuHzdEUnlZ" ,
+    
+
+}
